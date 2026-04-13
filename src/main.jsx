69a309cb791bf55d5149f013725aa095a1afb57d
masum634510-png <masum634510@gmail.com>
2026-04-13 20:08:30 +0600

    feat: setup router layout and navbar

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,38 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router'
+import Navbar from './component/Navbar/Navbar.jsx'
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+
+const router = createBrowserRouter([
+ {
+  path:"/",
+  element: <MainLayout></MainLayout>,
+  children : [
+     {
+    path:"/",
+    element:<div>welcome to my home</div>
+  },
+  {
+    path:"/contact",
+    element:<div>contact Me are you sucessful</div>
+  }
+  ]
+ }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </StrictMode>,
 )
