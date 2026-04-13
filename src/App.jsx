e2fd73739ab4d8cefe512f498ddbae15d6855e07
masum634510-png <masum634510@gmail.com>
2026-04-13 19:20:25 +0600

    init: setup react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-4xl text-red-400 text-center p-9'>This is a Masum khan</h1>
+    
+    </>
+  )
+}
+
+export default App
